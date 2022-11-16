f6719f9f1d9641f5bf4a481a858c31c9b480015d
Sophie Roudier <sophie.roudier@hotmail.fr>
2022-11-16 12:40:12 +0100

    Correcting issue with defining computer's choice on click

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// GET ELEMENTS
+// GETTING ELEMENTS
 
 const choiceList = document.querySelectorAll('.choice-btn')
 const choiceContainer = document.querySelector('.choice-container')
@@ -6,7 +6,7 @@ const gameContainer = document.querySelector('.game-container')
 
 
 
-// CREATE ELEMENTS
+// CREATING ELEMENTS
 
 const resultContainer = document.createElement('div');
 resultContainer.classList.add('result-container')
@@ -36,71 +36,31 @@ resultContainer.appendChild(gameResult)
 const computerChoiceContainer = document.createElement('div');
 computerChoiceContainer.classList.add('computer-choice-container');
 const computerChoiceBtn = document.createElement('div');
-computerChoiceContainer.innerHTML='<p>The house picked</p>';
+computerChoiceContainer.innerHTML = '<p>The house picked</p>';
 computerChoiceContainer.appendChild(computerChoiceBtn);
 const computerChoiceIcon = document.createElement('img');
 computerChoiceBtn.appendChild(computerChoiceIcon);
 resultContainer.appendChild(computerChoiceContainer)
 
-// Define user's choice
-let userChoice = "";
-let result;
-
-for (let choice of choiceList) {
-    choice.addEventListener('click', (e) => {
-        
-        userChoiceClicked = e.currentTarget
-        userChoice = e.currentTarget.id;
-        console.log(userChoice)
-        
-        gameMatch()
-
-        gameContainer.removeChild(choiceContainer)
-        gameContainer.appendChild(resultContainer)
-        userChoiceBtn.classList.add('user-choice', 'choice-btn', userChoice)
-        userChoiceBtn.innerHTML = userChoiceClicked.innerHTML;
-        
-        computerChoiceBtn.classList.add('computer-choice', 'choice-btn', computerChoice)
-        computerChoiceIcon.setAttribute('src', './images/icon-' + computerChoice + '.svg')
-        
-        document.querySelector('.result').innerText = result
-    })
-}
 
 
-//Define computer's choice
+// FUNCTIONS
 
-let computer = Math.random()
-let computerChoice = ""
+/* Defining the result and the score of the game */
 
-
-if (computer <= 1 / 3) {
-    computerChoice = "scissors";
-} else if (computer <= 2 / 3) {
-    computerChoice = "rock";
-} else {
-    computerChoice = "paper";
-}
-
-
-
-// Define result of the game
 let scoreNumber = document.querySelector('.score-number');
 scoreNumber.textContent = 0
 let score = 0
+let result;
 
-function gameMatch() {
+function displayResult() {
 
-    
-    
-    
-    
     switch (computerChoice) {
         case "scissors":
             if (userChoice === "rock") {
                 result = "You win";
                 score += 1
-                
+
             } else if (userChoice === "paper") {
                 result = "You lose";
             } else {
@@ -130,15 +90,55 @@ function gameMatch() {
     }
 
 
-scoreNumber.textContent = score    
-   
+    scoreNumber.textContent = score
+
 }
 
 
-// Restart the game
+// EVENTS LISTENERS
+
+let userChoice = "";
+let computerChoice = ""
+
+for (let choice of choiceList) {
+    choice.addEventListener('click', (e) => {
+
+        // Defining user's choice
+        userChoiceClicked = e.currentTarget
+        userChoice = e.currentTarget.id;
+        
+        /* Defining the computer's choice. */
+        let computer = Math.random()
+
+
+        if (computer <= 1 / 3) {
+            computerChoice = "scissors";
+        } else if (computer <= 2 / 3) {
+            computerChoice = "rock";
+        } else {
+            computerChoice = "paper";
+        }
+
+        displayResult()
+
+        gameContainer.removeChild(choiceContainer)
+        gameContainer.appendChild(resultContainer)
+        userChoiceBtn.classList.add('user-choice', 'choice-btn', userChoice)
+        userChoiceBtn.innerHTML = userChoiceClicked.innerHTML;
+
+        computerChoiceBtn.classList.add('computer-choice', 'choice-btn', computerChoice)
+        computerChoiceIcon.setAttribute('src', './images/icon-' + computerChoice + '.svg')
+
+        document.querySelector('.result').innerText = result
+    })
+}
+
+
+// Restarting the game
 
 resultBtn.addEventListener('click', () => {
     gameContainer.removeChild(resultContainer);
     gameContainer.appendChild(choiceContainer);
     userChoiceBtn.classList.remove(userChoice)
+    computerChoiceBtn.classList.remove(computerChoice)
 })
